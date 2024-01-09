bba0e8134ab3f54ee25f932db764cdf764b269ea
Isaac Campos <info@icampos.me>
2024-01-09 01:29:52 -0600

    Removed Docker from grun

---
+++ .wrangler/tmp/bundle-LvKv8i/middleware-insertion-facade.js
@@ -0,0 +1,3 @@
+import { __facade_registerInternal__ } from "/home/kthnkpd/.nvm/versions/node/v20.10.0/lib/node_modules/wrangler/templates/middleware/loader-sw.ts";
+import * as __MIDDLEWARE_0__ from "/home/kthnkpd/.nvm/versions/node/v20.10.0/lib/node_modules/wrangler/templates/middleware/middleware-miniflare3-json-error.ts";
+__facade_registerInternal__([__MIDDLEWARE_0__.default])
--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+const hostname = '0.0.0.0';
+const port = process.env.PORT || 2790;
+
 const Node = require('./src/node')();
 const config = require('./musiphone.config');
 const runner = require('./runner');
